af83d88f941f83acbe2f0f977f06217722e76a08
Alexey <alexeysh81@gmail.com>
2020-06-16 21:25:43 +0300

    2 class

--- project/project/Program.cs
+++ project/project/Program.cs
@@ -10,6 +10,12 @@ namespace project
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
+            Square s = new Square(2, 5, '*');
+            s.Draw();
+
+            Stick st = new Stick(6, 1, '*');
+            st.Draw();
+
             Point p1 = new Point(2, 3, '*');
             p1.Draw();
 
